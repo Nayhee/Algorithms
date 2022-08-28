a84b750b47c7de7148b58a13c90a85a6b7a11ddf
Nayhee <nathantraczewski@gmail.com>
2022-08-28 18:40:22 -0500

    symmetric tree

---
+++ SymmetricTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SymmetricTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsSymmetric(TreeNode root)
+            {
+                if (root.left == null && root.right == null) return true;
+                if(root.left == null || root.right == null) return false;
+                return Compare(root.left, root.right);
+            }
+
+            bool Compare(TreeNode left, TreeNode right)
+            {
+                if(left== null || right == null) return false;
+                if (left == null && right == null) return true;
+              
+
+        }
+    }
+}
