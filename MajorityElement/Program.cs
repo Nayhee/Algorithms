cceb5dcb74b3df02a144de0a0c0a022e32e64d99
Nayhee <nathantraczewski@gmail.com>
2022-09-01 11:27:10 -0500

    MajorityElement

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MajorityElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int MajorityElement(int[] nums)
+            {
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+
+                foreach(int num in nums)
+                {
+                    if(dict.ContainsKey(num))
+                    {
+                        dict[num]++;
+                    }
+                    else
+                    {
+                        dict.Add(num, 1);
+                    }
+                }
+
+                var maxValueKey = dict.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                return maxValueKey;
+            }
+
+            var nums = new int[7] { 2, 2, 1, 1, 1, 2, 2, };
+            Console.WriteLine(MajorityElement(nums));
+        }
+    }
+}
