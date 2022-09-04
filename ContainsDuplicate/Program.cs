1a2bcddd63636aeb042042fbd0b6bdabfb8cf86f
Nayhee <nathantraczewski@gmail.com>
2022-09-04 11:27:55 -0500

    contains duplicate AND HappyNumber

---
+++ ContainsDuplicate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainsDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ContainsDuplicate(int[] nums)
+            {
+                HashSet<int> uniqueNums = new HashSet<int>();
+
+                foreach(int num in nums)
+                {
+                    if (uniqueNums.Contains(num)) return false;
+                    else uniqueNums.Add(num);
+                }
+                return true;
+            }
+
+            var nums = new int[6] { 1, 3, 3, 5, 6, 7 };
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+    }
+}
---
+++ HappyNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HappyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsHappy(int n)
+            {
+
+                HashSet<int> seen = new HashSet<int>();
+                while (n != 1 && !seen.Contains(n))
+                {
+                    seen.Add(n);
+                    n = getNext(n);
+                }
+                return n == 1;
+            }
+
+            int getNext(int n)
+            {
+                int totalSum = 0;
+
+                while (n > 0)
+                {
+                    int d = n % 10;
+                    n = n / 10;
+                    totalSum += d * d;
+                }
+                return totalSum;
+            }
+
+            int n = 19;
+            Console.WriteLine(IsHappy(n));
+        }
+    }
+}
