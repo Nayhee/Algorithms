aeb2fad2936da6e079fda91e447f0cae57860f58
Nayhee <nathantraczewski@gmail.com>
2022-09-06 09:47:58 -0500

    linkedListCycle

---
+++ LinkedListCycle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LinkedListCycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool HasCycle(ListNode head)
+            {
+                if (head == null || head.next = null) return false;
+
+                ListNode first = head;
+                ListNode second = head.next;
+
+                while(first != second)
+                {
+                    if (first == null || second == null || first.next == null || second.next == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        first = first.next;
+                        second = second.next.next;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
