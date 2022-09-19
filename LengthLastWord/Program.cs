9cc1ce96db25a7a4c2bf07c3308c092d2287e72c
Nayhee <nathantraczewski@gmail.com>
2022-09-19 09:57:59 -0500

    added LengthLastWord

---
+++ LengthLastWord/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LengthLastWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int LengthLastWord(string s)
+            {
+                string[] words = s.Split(' ');
+                return words[words.Length - 1].Length;
+            }
+            string s = "Hello World";
+            Console.WriteLine(LengthLastWord(s));
+        }
+    }
+}
